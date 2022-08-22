e66d7bcfbbee8e41474d11d60bda4a3230a96940
James Boland <james@jboland.tech>
2022-08-22 22:44:27 +0100

    We can now move the pitures from a whole directory + click added

--- rename_files_from_metadata.py
+++ rename_files_from_metadata.py
@@ -2,10 +2,19 @@ from PIL import Image, ExifTags
 from pathlib import Path
 import datetime as dt 
 from shutil import copyfile
+import click
 
 IMG_FILE_TYPES = {".JPG", ".jpg", ".bmp"}
 VID_FILE_TYPES = {".MP4", ".mp4"}
 FILE_NAME_TS_FORMAT = "%Y-%m-%d|%H-%M-%S"
+
+@click.command()
+@click.argument('src_dir', type=Path)
+@click.argument('dest_dir', type=Path)
+def copy_and_reorganise(src_dir, dest_dir):
+    for path in src_dir.iterdir():
+        copy_file_using_timestamp(path, dest_dir)
+
 
 def copy_file_using_timestamp(src_file, dest_dir):
     new_sub_path = get_new_path_for_file(src_file)
@@ -25,8 +34,6 @@ def get_file_details(src_file):
     src_extension = src_file.suffix
     if src_extension in IMG_FILE_TYPES:
         return "img", get_timestamp_from_img_path(src_file)
-    elif src_extension in VID_FILE_TYPES:
-        return "vid", get_timestamp_from_vid_path(src_file)
     else:
         raise TypeError(f"\"{src_extension}\" is not an accepted file extension")
 
@@ -46,20 +53,11 @@ def get_exif_dict(img):
     return {ExifTags.TAGS[k]: v for k, v in img._getexif().items() if k in ExifTags.TAGS}
 
 
-def get_timestamp_from_vid_path(path):
-    pass
-
-
 def copy_to_destination(src_file, new_file):
     dest_dir = new_file.parent
-    dest_dir.mkdir(parents=True)
+    dest_dir.mkdir(parents=True, exist_ok=True)
     copyfile(src_file, new_file)
 
 
 if __name__ == "__main__":
-    root = Path(__file__).parent
-    test_file = root / "temp_src" / "IMG_0001.JPG"
-    test_out = root / "temp_dest"
-    print(test_file)
-    print(test_file.exists())
-    copy_file_using_timestamp(test_file, test_out)
+    copy_and_reorganise()

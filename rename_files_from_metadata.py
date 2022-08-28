a842ee875b5a24df9920182c345cf256eb053015
James Boland <james@jboland.tech>
2022-08-28 15:49:16 +0100

    We can now handle videos + added logging

--- rename_files_from_metadata.py
+++ rename_files_from_metadata.py
@@ -7,22 +7,26 @@ from shutil import copyfile, rmtree
 from tqdm import tqdm
 from hashlib import sha1
 import click
+import logging
+
+logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.INFO)
 
 IMG_FILE_TYPES = {".JPG", ".jpg", ".bmp"}
 VID_FILE_TYPES = {".MP4", ".mp4"}
 FILE_NAME_TS_FORMAT = "%Y-%m-%d|%H-%M-%S"
 
 @click.command()
-@click.argument('src_dir', type=Path)
-@click.argument('dest_dir', type=Path)
-def copy_and_reorganise(src_dir, dest_dir):
+@click.argument("src_dir", type=Path)
+@click.argument("dest_dir", type=Path)
+@click.argument("src_name", type=str)
+def copy_and_reorganise(src_dir, dest_dir, src_name):
     all_files = list(src_dir.iterdir())
 
     if dest_dir.exists() and not delete_directory_with_prompt(dest_dir):
         raise FileExistsError("The destination directory already exists!")
 
     for path in tqdm(all_files, total=len(all_files)):
-        copy_file_using_timestamp(path, dest_dir)
+        copy_file_using_timestamp(path, dest_dir, src_name)
     
     check_files(dest_dir, src_dir)
 
@@ -49,18 +53,17 @@ def positive_ints():
         yield num
 
 
-def copy_file_using_timestamp(src_file, dest_dir):
-    new_sub_path = get_new_path_for_file(src_file)
+def copy_file_using_timestamp(src_file, dest_dir, src_name):
+    new_sub_path = get_new_path_for_file(src_file, src_name)
     copy_to_destination(src_file, dest_dir / new_sub_path)
 
 
-def get_new_path_for_file(src_file):
+def get_new_path_for_file(src_file, src_name):
     file_type, file_timestamp = get_file_details(src_file)
 
     timestamp_str = file_timestamp.strftime(FILE_NAME_TS_FORMAT)
-    new_name = f"{timestamp_str}_{file_type}_from_camera{src_file.suffix}"
-    sub_path_suffix = "" if file_type == "img" else f"_{file_type}"
-    new_path = file_timestamp.strftime("%Y-%m") + sub_path_suffix
+    new_name = f"{timestamp_str}_{file_type}_from_{src_name}{src_file.suffix}"
+    new_path = file_timestamp.strftime("%Y-%m") + f"_{file_type}"
     return Path(new_path) / new_name
 
 
@@ -86,9 +89,9 @@ def get_timestamp_from_img(img):
 
 
 def get_timestamp_from_vid_path(src_file):
+    orig_timestamp_str = probe(src_file)["format"]["tags"]["creation_time"]
+    return dt.datetime.fromisoformat(orig_timestamp_str.rstrip("Z"))
     
-    
-
 
 def get_exif_dict(img):
     return {ExifTags.TAGS[k]: v for k, v in img._getexif().items() if k in ExifTags.TAGS}
@@ -114,6 +117,7 @@ def hash_file(path):
 
 
 def check_files(dest_dir, src_dir):
+    logging.info("Validating copy...")
     src_hash_dict = get_hashes(src_dir)
     dest_hash_dict = get_hashes(dest_dir)
 
@@ -121,21 +125,19 @@ def check_files(dest_dir, src_dir):
     dest_hashes = set(dest_hash_dict.keys())
 
     if src_hashes == dest_hashes:
-        print(f"{len(dest_hashes)}/{len(src_hashes)} reorganised successfully")        
+        logging.info(f"{len(dest_hashes)}/{len(src_hashes)} reorganised successfully")        
         return True
     
-    print("Files missing!!")
+    logging.info("Files missing!!")
     missing_from_dest = {src_hash_dict[hash] for hash in src_hashes.difference(dest_hashes)}
     if missing_from_dest:
-        print("Missing from destination: ")
-        print("\n\t".join([str(path) for path in missing_from_dest]))
+        logging.info("Missing from destination: ")
+        logging.info("\n\t".join([str(path) for path in missing_from_dest]))
     
     missing_from_src = {dest_hash_dict[hash] for hash in dest_hashes.difference(src_hashes)}
     if missing_from_src:
-        print("Missing from source: ")
-        print("\n\t".join([str(path) for path in missing_from_src]))
-
-
+        logging.info("Missing from source: ")
+        logging.info("\n\t".join([str(path) for path in missing_from_src]))
 
 
 def get_hashes(dir):
@@ -146,7 +148,6 @@ def get_hashes(dir):
         f"num files: {len(files)}, num hashes: {len(hashes.keys())}"
     )
     return hashes
-
 
 
 if __name__ == "__main__":
